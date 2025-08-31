a726f27a9f15b69b3373630bfdd9b73fe1e0ba71
matin mirakhori <138450964+Matincrypto@users.noreply.github.com>
2025-09-01 00:20:45 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 # config.py
 
-COINEX_ACCESS_ID = "00866D8CC9B04F2BA01901D93544B3A2"
-COINEX_SECRET_KEY = "9C186ADBD701CF8311E5F1E0EB8A6100EC36EC32F5D6B8DE"
+COINEX_ACCESS_ID = "-+++++++++++-"
+COINEX_SECRET_KEY = "-+++++++++++-"
 
 # --- Trade Settings ---
 # The amount in USDT to use as margin for each trade.
 USDT_AMOUNT = 10
 
 # The leverage to be used for all trades.
+
 LEVERAGE = 0
